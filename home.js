ade0e07145f1096ea11a551e2e49ac8781bf5aad
Maria <84650183+mgraublanco@users.noreply.github.com>
2022-12-07 00:53:25 -0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,18 @@
+function Home() {
+    return (
+      <>
+        <Card
+          txtcolor="black"
+          header="Welcome to FastFinance"
+          title="Achieve financial freedom with FastFinance"
+          text="Open an account, Deposit and Withdraw funds wherever you are!"
+          body={(
+            <>
+              <p className="text-center"><img src="./ff.png" className="img-fluid" alt="ff-logo.png"/></p>
+      
+            </>
+            )}
+        /> 
+      </> 
+    );  
+  }
